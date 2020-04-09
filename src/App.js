8dac58a2564a3afe7acb3630a196cdc64af36a10
Oksana <Oksana>
2020-04-09 18:08:37 +0300

    added popular card

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
-import { hot } from 'react-hot-loader/root';
-import Ads from './ads/index.jsx';
+import { hot } from "react-hot-loader/root";
+import Ads from "./ads/index.jsx";
+import Popular from "./popular/index.jsx";
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <Ads />
+        <Popular />
       </div>
     );
   }

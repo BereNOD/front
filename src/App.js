05fb591486e7dec91e4a2a0e5c0f337be78b2b22
Veronika <veronika.nagurskaya@gmail.com>
2020-04-09 19:41:43 +0300

    banner

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { hot } from 'react-hot-loader/root';
 import Ads from './ads/index.jsx';
+import Banner from './banner/banner.jsx';
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <Ads />
+        <Banner />
       </div>
     );
   }

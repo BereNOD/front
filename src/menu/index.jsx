99577337e3120cedff7dbe6b2c65e0b8fae4273a
TimofeevTim <timofeevtg@gmail.com>
2020-04-14 18:28:11 +0300

    ReadyToVadim

--- src/menu/index.jsx
+++ src/menu/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import MenuImage from './imageMenu/cat-icon.png';
-// import MenuImage1 from './imageMenu/cat-icon.png';
-// import MenuImage2 from './imageMenu/cat-icon.png';
-// import MenuImage3 from './imageMenu/cat-icon.png';
-// import MenuImage4 from './imageMenu/cat-icon.png';
-// import MenuImage5 from './imageMenu/cat-icon.png';
+import MenuImage from './imageMenu/cat-icon.svg';
+import MenuImage1 from './imageMenu/dog-icon.png';
+import MenuImage2 from './imageMenu/mouse-icon.png';
+import MenuImage3 from './imageMenu/fish-icon.png';
+import MenuImage4 from './imageMenu/chicken-icon.png';
+import MenuImage5 from './imageMenu/pig-icon.png';
 
 import './styles.scss';
 
@@ -14,27 +14,27 @@ class Menu extends React.Component {
         <section className="sectionMenu">
                     <div className="menu">
                         <div className="categoryAnimal firstLeftAnimal" title="Кошки">
-                            <img src={MenuImage} alt="" className="imageIcon" />
+                            <img src={MenuImage} alt="" className="imageIcon" width="33px" height="35px" />
                             <span className="imageText">Кошки</span>
                         </div>
                         <div className="categoryAnimal" title="Собаки">
-                            <img src={MenuImage} alt="" className="imageIcon" />
+                            <img src={MenuImage1} alt="" className="imageIcon" width="33px" height="35px" />
                             <span className="imageText">Собаки</span>
                         </div>
                         <div className="categoryAnimal" title="Грызуны">
-                            <img src={MenuImage} alt="" className="imageIcon" />
+                            <img src={MenuImage2} alt="" className="imageIcon" width="33px" height="35px" />
                             <span className="imageText">Грызуны</span>
                         </div>
                         <div className="categoryAnimal" title="Рыбы">
-                            <img src={MenuImage} alt="" className="imageIcon" />
+                            <img src={MenuImage3} alt="" className="imageIcon" width="33px" height="35px" />
                             <span className="imageText">Рыбы</span>
                         </div>
                         <div className="categoryAnimal" title="Птицы">
-                            <img src={MenuImage} alt="" className="imageIcon" />
+                            <img src={MenuImage4} alt="" className="imageIcon" width="33px" height="35px" />
                             <span className="imageText">Птицы</span>
                         </div>
                         <div className="categoryAnimal" title="Сельхоз Животные">
-                            <img src={MenuImage} alt="" className="imageIcon" />
+                            <img src={MenuImage5} alt="" className="imageIcon" width="33px" height="35px" />
                             <span className="imageText">Сельхоз Животные</span> 
                         </div>
                     </div>

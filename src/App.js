0b761249711d080af68f1497963b6055496714e9
AtomicJedi <AtomicJedi@i.ua>
2020-04-17 17:54:01 +0300

    header2.0.1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { hot } from 'react-hot-loader/root';
+import Header from './header/header.jsx';
 import Ads from './ads/index.jsx';
 import Banner from './banner/banner.jsx';
 import New from './new/new.jsx';
@@ -8,6 +9,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
+        <Header />
         <Ads />
         <Banner />
         <New />
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import App from "./App.js";
 import "./styles.scss";
 import "./styles.less";
+import "./header/header.jsx"
 
 ReactDOM.render(
   <App name="Jane" />,

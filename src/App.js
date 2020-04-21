d87a6cc01cc43a5e78bd21f36c684ef7322022c9
Dezz1927 <dezz1928@gmail.com>
2020-04-21 14:42:52 +0300

    Sale

--- src/App.js
+++ src/App.js
@@ -8,23 +8,10 @@ import New from './new/new.jsx';
 
 
 class App extends React.Component {
-  state = {
-    posts: []
-  };
-  componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-  .then(response => response.json())
-  .then(albums => this.setState({ posts: posts }))
-  }
+
   render() {
     return (
-      <div className="main">
-      {_.map(this.state.posts, post => (
-        <div className="mainPost">
-        <h2>{post.title}</h2>
-        <p>{post.body}</p>
-        </div>
-      ))}
+      <div>
            <Ads />
            <Banner />
            <Sale />

ef9ec908c3b32ec40105c9c8e97abc9c97a88753
Dezz1927 <dezz1928@gmail.com>
2020-04-21 14:12:31 +0300

    Sale

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import React from "react";
 import { hot } from 'react-hot-loader/root';
+import _ from 'lodash'
 import Ads from './ads/index.jsx';
 import Banner from './banner/banner.jsx';
-<<<<<<< HEAD
 import Sale from './Sale/Sale.jsx'
-=======
 import New from './new/new.jsx';
->>>>>>> master
+
 
 class App extends React.Component {
+  state = {
+    posts: []
+  };
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+  .then(response => response.json())
+  .then(albums => this.setState({ posts: posts }))
+  }
   render() {
     return (
-      <div>
-        <Ads />
-        <Banner />
-<<<<<<< HEAD
-        <Sale />
-=======
-        <New />
->>>>>>> master
+      <div className="main">
+      {_.map(this.state.posts, post => (
+        <div className="mainPost">
+        <h2>{post.title}</h2>
+        <p>{post.body}</p>
+        </div>
+      ))}
+          // <Ads />
+          // <Banner />
+          // <Sale />
+           // <New />
       </div>
     );
   }
--- webpack.config.js
+++ webpack.config.js
@@ -1,132 +1,120 @@
-const webpack = require('webpack');
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const LodashModuleReplacementPlugin = require('lodash-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const LodashModuleReplacementPlugin = require("lodash-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const config = {
-
-  entry: [
-    'react-hot-loader/patch',
-    './src/index.js'
-  ],
-
   devtool: 'eval-source-map',
   entry: ["react-hot-loader/patch", "./src/index.js"],
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].[contenthash].js'
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].[contenthash].js",
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        use: 'babel-loader',
-        exclude: /node_modules/
+        use: "babel-loader",
+        exclude: /node_modules/,
       },
       {
         test: /\.css$/,
         use: [
           MiniCssExtractPlugin.loader,
           {
-            loader: 'css-loader',
-            options: {
-              importLoaders: 1
-            }
-          },
-          'postcss-loader'
-        ],
-        exclude: /\.module\.css$/
-      },
-      {
-        test: /\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader',
+            loader: "css-loader",
             options: {
               importLoaders: 1,
-              modules: true
-            }
+            },
           },
-          'postcss-loader'
+          "postcss-loader",
         ],
-        include: /\.module\.css$/
+        exclude: /\.module\.css$/,
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
+              modules: true,
+            },
+          },
+          "postcss-loader",
+        ],
+        include: /\.module\.css$/,
       },
       {
         test: /\.scss$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          'sass-loader'
-        ]
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
       },
       {
         test: /\.less$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          'less-loader'
-        ]
+        use: [MiniCssExtractPlugin.loader, "css-loader", "less-loader"],
       },
       {
         test: /\.svg$/,
-        use: 'file-loader'
+        use: "file-loader",
       },
       {
         test: /\.png$/,
         use: [
           {
-            loader: 'url-loader',
+            loader: "url-loader",
             options: {
-              mimetype: 'image/png'
-            }
-          }
-        ]
-      }
-    ]
+              mimetype: "image/png",
+            },
+          },
+        ],
+      },
+    ],
   },
   resolve: {
-    extensions: [
-      '.js',
-      '.jsx'
-    ],
+    extensions: [".js", ".jsx"],
     alias: {
-      'react-dom': '@hot-loader/react-dom'
-    }
-  },
-  devServer: {
-    contentBase: './dist'
+      "react-dom": "@hot-loader/react-dom",
+    },
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: require('html-webpack-template'),
-      inject: false,
-      appMountId: 'app',
-      filename: 'index.html'
+      template: require("html-webpack-template"),
+      inject: true,
+      appMountId: "app",
     }),
     new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /en/),
-    new LodashModuleReplacementPlugin,
-    new MiniCssExtractPlugin()
+    new LodashModuleReplacementPlugin(),
+    new MiniCssExtractPlugin(),
   ],
+  devServer: {
+    contentBase: "./dist",
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": "true",
+      "Access-Control-Allow-Headers": "Content-Type, Authorization, x-id, Content-Length, X-Requested-With",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS"
+    }
+  },
   optimization: {
-    runtimeChunk: 'single',
+    runtimeChunk: "single",
     splitChunks: {
       cacheGroups: {
         vendor: {
           test: /[\\/]node_modules[\\/]/,
-          name: 'vendors',
-          chunks: 'all'
-        }
-      }
-    }
-  }
+          name: "vendors",
+          chunks: "all",
+        },
+      },
+    },
+  },
 };
 
 module.exports = (env, argv) => {
   if (argv.hot) {
     // Cannot use 'contenthash' when hot reloading is enabled.
-    config.output.filename = '[name].[hash].js';
+    config.output.filename = "[name].[hash].js";
   }
 
   return config;

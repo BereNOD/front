3cb5f099eab40c0a6b9f1ca6cb6515fb5b41344d
TimofeevTim <timofeevtg@gmail.com>
2020-04-21 19:35:53 +0300

    Comments

--- src/App.js
+++ src/App.js
@@ -3,14 +3,29 @@ import { hot } from 'react-hot-loader/root';
 import Ads from './ads/index.jsx';
 import Banner from './banner/banner.jsx';
 import Menu from './menu/index.jsx';
+import Comments from './Comments/Comments'
+
+const data = [
+  {
+    "postId": 1,
+    "id": 1,
+    "name": "id labore ex et quam laborum",
+    "email": "Eliseo@gardner.biz",
+    "body": "laudantium enim quasi est quidem magnam voluptate ipsam eos\ntempora quo necessitatibus\ndolor quam autem quasi\nreiciendis et nam sapiente accusantium"
+  }
+]
+
+
+
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <Ads />
+      <Comments data = {data}/>
+        {/* <Ads />
         <Banner />
-        <Menu />
+        <Menu /> */}
       </div>
     );
   }

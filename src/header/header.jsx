2dce5728c89021c3ac41694dac776cc50ed61142
AtomicJedi <AtomicJedi@i.ua>
2020-04-30 20:13:29 +0300

    header2.0.3

--- src/header/header.jsx
+++ src/header/header.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
 
 import logo from "./kenguru/logo.png";
 import phone_icon from "./kenguru/phone_icon.png";
@@ -7,21 +6,11 @@ import location_icon from "./kenguru/location_icon.png";
 import user_icon from "./kenguru/user_icon.png";
 import cart_icon from "./kenguru/cart_icon.png";
 import './header.scss';
-=======
-import background_image 'kenguru/back.png';
-import logo 'kenguru/logo.png';
-import phone_icon 'kenguru/phone_icon.png';
-import location_icon 'kenguru/location_icon';
-import user_icon 'kenguru/user_icon';
-import cart_icon 'kenguru/cart_icon'
-import 'header.scss';
->>>>>>> 60d89e135632bcc7139ce3aac094bc9ce2cdf41f
 
 class header extends React.Component {
   render() {
     return (
       <div className="Header">
-<<<<<<< HEAD
       <div className="Top_header">
         <div className="Logo_Wrapper">
           <img src={logo}/>
@@ -63,40 +52,3 @@ class header extends React.Component {
 
 
 export default header;
-=======
-        <div className="Top_header">
-          <div className="Logo_Wrapper">
-            <img src={logo} />
-        </div>
-
-      <div className="Contacts">
-        <div className="Telnumber">
-          <img src={phone_icon} />
-          <a href="#">+7(930) 635 65 25</a>
-        </div>
-          <div className="location_icon">
-            <img src={location_icon} />
-            <a href="#">г.Тула,ул.Пузакова,44</a>
-          </div>
-      </div>
-
-      <div className="Search">
-           <form class="form-inline my-2 my-lg-0">
-                <input id="Search_product" class="form-control mr-sm-2" type="search" placeholder="Поиск по товарам" aria-label="Search">
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Найти</button>
-           </form>
-      </div>
-
-      <div className="Profile">
-         <img src={user_icon} />
-            <a href="#" style="color: #68D6F4" className="Profile_link">Личный кабаинет</a>
-            </div>
-
-      <div className="Cart">
-         <img src={cart_icon} />
-         <a href="#" style="color: #68D6F4" className="Cart_link">Корзина</a>
-            </div>
-
-      </div>
-      }
->>>>>>> 60d89e135632bcc7139ce3aac094bc9ce2cdf41f
--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-<<<<<<< HEAD
 import App from "./App.js";
 import "./styles.scss";
 import "./styles.less";
 import "./header/header.jsx"
-=======
-import App from "./App";
-import "./styles.scss";
-import "./styles.less";
->>>>>>> 60d89e135632bcc7139ce3aac094bc9ce2cdf41f
 
 ReactDOM.render(
   <App name="Jane" />,
